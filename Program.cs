a9bb396c656cbf7ac3ab8d38734d3a3621773471
Grin <grinlikegreenbutgrin@gmail.com>
2019-05-23 22:48:09 +0500

    Task

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 4, 0, 3, 19, 492, -10, 1 };
+            int result = Calc(arr);
+            Console.WriteLine(result);
+        }
+
+        static int Calc(IEnumerable<int> arr)
+        {
+            int firstMin = int.MaxValue, secondMin = int.MaxValue;
+            foreach(int e in arr)
+            {
+                if(e > secondMin) continue;
+                if(e < firstMin)
+                {
+                    secondMin = firstMin;
+                    firstMin = e;
+                }
+                else
+                {
+                    secondMin = e;
+                }
+            }
+            return firstMin + secondMin;
+        }
+    }
+}
